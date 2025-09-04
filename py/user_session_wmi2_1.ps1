ee14149752737646c75e2930162ef419e33a5e3a
aropl22 <arobrooklyn@gmail.com>
2025-09-04 15:27:06 -0400

    1

---
+++ py/user_session_wmi2_1.ps1
@@ -0,0 +1,27 @@
+# Log file location
+$LogFile = "C:\Logs\ActiveUsers.log"
+
+# Get timestamp
+$TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+# Query WMI for console user
+$UserInfo = Get-CimInstance Win32_ComputerSystem
+
+# Collect usernames
+$Users = @()
+foreach ($u in $UserInfo) {
+    $username = $u.UserName
+    if ([string]::IsNullOrEmpty($username)) {
+        $username = "No user logged in"
+    } else {
+        # Remove domain if present
+        $username = $username.Split('\')[-1]
+    }
+    $Users += $username
+}
+
+# Prepare log line
+$LogLine = "$TimeStamp : $($Users -join ',')"
+
+# Write to log
+Add-Content -Path $LogFile -Value $LogLine
